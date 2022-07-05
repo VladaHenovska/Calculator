d6768ce306ea70ef4e85766c1ed22fd3298811e8
Vlada Henovska <vhenovska@gmail.com>
2022-07-05 20:40:30 +0300

    Once button pressed text label is updated

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,10 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberTextLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        numberTextLabel.text = ""
     }
+    
+    @IBAction func operationPressed(_ sender: UIButton) {
+        if let operation = sender.currentTitle{
+            numberTextLabel.text?.append(operation)
+            if operation == "="{
+                numberTextLabel.text = ""
+            }
+        }
+        
+        
+    }
+    
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+        if let digit = sender.currentTitle{
+            numberTextLabel.text?.append(digit)
+        }
+    }
+    
 
 
 }
